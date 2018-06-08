61e59504a4eaac69af8daaa1697ee3f71a9e3d54
chainsx(cx-dandelion) <chainsx@users.noreply.github.com>
2018-06-08 16:23:11 +0800

    Update makeimage.sh

--- build/makeimage.sh
+++ build/makeimage.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 sudo apt-get install -y dosfstools dump parted kpartx
 
-sudo dd if=/dev/zero of=raspberrypi.img bs=1M count=2100
-sudo parted raspberrypi.img mktablel msdos
-sudo parted raspberrypi.img --script -- mkpart primary fat32 8192s 2682879s
-sudo parted raspberrypi.img --script -- mkpart primary ext4 2682880s -1
+sudo dd if=/dev/zero of=image.img bs=1M count=2100
+sudo parted image.img mktablel msdos
+sudo parted image.img --script -- mkpart primary fat32 8192s 2682879s
+sudo parted image.img --script -- mkpart primary ext4 2682880s -1
 
-sudo kpartx raspberrypi.img
+sudo kpartx image.img
 partBoot= $(ls /dev/mapper | grep p1)
 partRoot= $(ls /dev/mapper | grep p2)
 sudo mkfs.vfat $partBoot
 sudo mkfs.ext4 $partRoot
-sudo mount -t vfat $partBoot /mnt
-sudo cp -rfp boot/* /mnt
-sudo umount /media
-sudo mount -t ext4 $partRoot /media/
-cd /media
-sudo dump -0uaf - / | sudo restore -rf -
-cd
-sudo umount /media
-sudo kpartx -d $loopdevice
-sudo losetup -d $loopdevice
+sudo mount $partRoot /mnt
+sudo cp -rfp rootfs/* /mnt
+sudo mount $partBoot /mnt/boot
+sudo cp -rfp boot/* /mnt/boot
+sudo umount /mnt/boot
+sudo umount /mnt
+sudo sync
+sudo kpartx -d image.img
