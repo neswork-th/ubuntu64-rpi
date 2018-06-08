7adf7050ad40f012ffb4f5d608dca98719bdf07a
chainsx(cx-dandelion) <chainsx@users.noreply.github.com>
2018-06-08 16:59:54 +0800

    Update step2.sh

--- build/step2.sh
+++ build/step2.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
-echo "Start Step2...."
 cd build
+D= $(pwd)
 mkdir boot
-sudo apt-get install git
 sudo git clone --depth 1 stable https://github.com/raspberrypi/firmware.git
-cd firmware/boot
-cp * ../..
-sudo rm *.dtb
-sudo rm *kernel*
-cd ../..
+cp-r firmware/boot/* $D/boot
+sudo rm $D/boot/*.dtb
+sudo rm $D/boot/*kernel*
 sudo rm -rf firmware
-cd ../rootfs/lib
+cd $D/rootfs/lib
 sudo git clone --depth 1 https://github.com/rpi-distro/firmware-nonfree.git
 sudo mv firmware-nonfree firmware
-cd firmware
-sudo rm .git
-cd ../..
-mkdir init-firmware
-cd init-firmware
-sudo apt-get download linux-headers-raspi2 linux-image-raspi2
-sudo dpkg -X *.deb
+sudo rm firmware/.git
+cd $D
