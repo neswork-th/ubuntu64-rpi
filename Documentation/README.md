29b489ff5fb89102c4b16efb402e2620ed0a70f2
chainsx <pzhrhlx@outlook.com>
2018-12-16 10:14:58 +0800

    push

--- Documentation/README.md
+++
@@ -1,1 +0,0 @@
-如果你需要帮助请[点这里](https://github.com/chainsx/ubuntu64-rpi/wiki)
--- build/build-kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wget https://github.com/raspberrypi/linux/archive/rpi-4.11.y.zip
-unzip rpi*
-cd linux*
-make bcmrpi3_defconfig CROSS_COMPILE=aarch64-linux-gnu-
-make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
